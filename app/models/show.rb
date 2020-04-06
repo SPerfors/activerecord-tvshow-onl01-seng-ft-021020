767dbcd91aa0bb590d7b9ef757a16c471707e378
Steven Perfors <stevecanbuild@gmail.com>
2020-04-06 20:55:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,4 +8,14 @@ class Show < ActiveRecord::Base
     Show.order(rating: :desc).first
   end 
   
+  def Show::lowest_rating
+    Show.minimum("rating")
+  end 
+  
+  def Show::least_popular_show 
+    Show.order(rating: :asc).first 
+  end
+  
+  def Show::ratings_sum 
+    Show.
 end
